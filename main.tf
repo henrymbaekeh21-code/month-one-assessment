3a5cdf7a85f0a08d16a5301ab3a2c75485bd2bcc
henry Mba-Ekeh <henryscholes@henrys-MacBook-Air.local>
2026-04-19 14:08:01 +0100

    add networking - VPC, subnets, IGW, NAT gateways, route tables

--- main.tf
+++ main.tf
@@ -11,3 +11,72 @@ terraform {
 provider "aws" {
   region = var.region
 }
+# ========================
+# VPC
+# ========================
+resource "aws_vpc" "techcorp_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "techcorp-vpc"
+  }
+}
+# ========================
+# PUBLIC SUBNETS
+# ========================
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.techcorp_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.region}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "techcorp-public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.techcorp_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "${var.region}b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "techcorp-public-subnet-2"
+  }
+}
+
+# ========================
+# PRIVATE SUBNETS
+# ========================
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.techcorp_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "${var.region}a"
+
+  tags = {
+    Name = "techcorp-private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.techcorp_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "${var.region}b"
+
+  tags = {
+    Name = "techcorp-private-subnet-2"
+  }
+}
+# ========================
+# INTERNET GATEWAY
+# ========================
+resource "aws_internet_gateway" "techcorp_igw" {
+  vpc_id = aws_vpc.techcorp_vpc.id
+
+  tags = {
+    Name = "techcorp-igw"
+  }
+}
